8adaca8c629de604bb70b8cf0b637ddf825fde4c
Marcin <marcinsiekierski97@o2.pl>
2018-09-04 19:07:40 +0200

    ..

--- DreamsComeTrueSPA/src/app/app.module.ts
+++ DreamsComeTrueSPA/src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TodoItemsComponent } from './todo-items/todo-items.component';
-import { HttpClientModule } from '@angular/common/http';
+import { NavComponent } from './nav/nav.component';
+
+import { AuthService } from './_services/auth.service';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
    declarations: [
       AppComponent,
-      TodoItemsComponent
+      TodoItemsComponent,
+      NavComponent,
+      HomeComponent,
+      RegisterComponent
    ],
    imports: [
       BrowserModule,
-      HttpClientModule
+      HttpClientModule,
+      FormsModule
    ],
-   providers: [],
+   providers: [
+      AuthService
+   ],
    bootstrap: [
       AppComponent
    ]
