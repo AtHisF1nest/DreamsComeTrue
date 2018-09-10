aa8953b2aaaf81446bb86359ea31ade585233654
Marcin Siekierski OPONEO.PL S.A <Marcin.Siekierski@oponeo.pl>
2018-09-10 19:22:54 +0200

    app moduel

--- DreamsComeTrueSPA/src/app/app.module.ts
+++ DreamsComeTrueSPA/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { BsDropdownModule } from 'ngx-bootstrap';
 import { RouterModule } from '@angular/router';
+import { JwtModule } from '@auth0/angular-jwt';
 
 import { AuthService } from './_services/auth.service';
 import { AlertifyService } from './_services/alertify.service';
@@ -20,6 +21,10 @@ import { appRoutes } from './routes';
 import { AuthGuard } from './_guards/auth.guard';
 import { TodosService } from './_services/todos.service';
 
+export function tokenGetter() {
+    return localStorage.getItem('token');
+}
+
 @NgModule({
    declarations: [
       AppComponent,
@@ -35,7 +40,14 @@ import { TodosService } from './_services/todos.service';
       HttpClientModule,
       FormsModule,
       BsDropdownModule.forRoot(),
-      RouterModule.forRoot(appRoutes)
+      RouterModule.forRoot(appRoutes),
+      JwtModule.forRoot({
+          config: {
+              tokenGetter: tokenGetter,
+              whitelistedDomains: ['localhost:5000'],
+              blacklistedRoutes: ['localhost:5000/api/auth']
+          }
+      })
    ],
    providers: [
       AuthService,
